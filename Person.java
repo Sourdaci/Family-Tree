aa4c5d095ad746061a856dc80a60865d51164815
Sourdaci <sourdaci@gmail.com>
2015-01-07 16:07:03 +0100

    Family Tree basico: atributos y constructor

---
+++ Person.java
@@ -0,0 +1,21 @@
+
+/**
+ * Persona sobre la que trabajar
+ */
+public class Person
+{
+    // Nombre de la persona
+    private String name;
+    // Edad de la persona
+    private int age;
+
+    /**
+     * Nueva persona para el programa
+     * El nombre es una cadena de texto
+     * La edad es un numero entero
+     */
+    public Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+}
